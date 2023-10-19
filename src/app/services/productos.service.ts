ff15038d6e5e5d5b4f2e3b559908649e166622b8
Enric Alcaraz Jornet <ealcaraz@mediphealth.com>
2023-10-19 13:23:12 +0200

    products

--- src/app/services/productos.service.ts
+++ src/app/services/productos.service.ts
@@ -1,5 +1,8 @@
-import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Producto } from '../interfaces/producto.interface';
+
+
 
 @Injectable({
   providedIn: 'root'
@@ -7,17 +10,75 @@ import { Injectable } from '@angular/core';
 export class ProductosService {
 
   cargando = true;
+  productos: Producto[] = [];
+  productosFiltrado: Producto[] = [];
 
-  constructor( private http: HttpClient) {
+
+  constructor( private http: HttpClient ) {
+
     this.cargarProductos();
-   }
-   private cargarProductos(){
-    this.http.get('https://portafolio-52950-default-rtdb.europe-west1.firebasedatabase.app/productos_idx.json')
-    .subscribe((resp: any) =>{
-      // this.cargada = true;
-      // this.equipo = resp;
-      console.log(resp);
-      this.cargando = false;
+
+  }
+
+
+  private cargarProductos() {
+
+    return new Promise<void>(  ( resolve, reject ) => {
+
+      this.http.get('https://angular-html-25cf9.firebaseio.com/productos_idx.json')
+          .subscribe( (resp: any) => {
+            this.productos = resp;
+            this.cargando = false;
+            resolve();
+          });
+
     });
-   }
+
+  }
+
+  getProducto( id: string ) {
+
+    return this.http.get(`https://angular-html-25cf9.firebaseio.com/productos/${ id }.json`);
+
+  }
+
+  buscarProducto( termino: string ) {
+
+
+    if ( this.productos.length === 0 ) {
+      // cargar productos
+      this.cargarProductos().then( () => {
+        // ejecutar después de tener los productos
+        // Aplicar filtro
+        this.filtrarProductos( termino );
+      });
+
+    } else {
+      // aplicar el filtro
+      this.filtrarProductos( termino );
+    }
+
+
+  }
+
+  private filtrarProductos( termino: string ) {
+
+    // console.log(this.productos);
+    this.productosFiltrado = [];
+
+    termino = termino.toLocaleLowerCase();
+
+    this.productos.forEach( prod => {
+
+      const tituloLower = prod.titulo.toLocaleLowerCase();
+
+      if ( prod.categoria.indexOf( termino ) >= 0 || tituloLower.indexOf( termino ) >= 0  ) {
+        this.productosFiltrado.push( prod );
+      }
+
+    });
+
+
+  }
+
 }
--- src/app/shared/header/header.component.ts
+++ src/app/shared/header/header.component.ts
@@ -1,16 +1,22 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 import { InfoPaginaService } from 'src/app/services/info-pagina.service';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styleUrls: ['./header.component.css']
+  styleUrls: ['./header.component.css'],
 })
 export class HeaderComponent implements OnInit {
+  constructor(public _servicio: InfoPaginaService, private router: Router) {}
 
-  constructor( public _servicio: InfoPaginaService) { }
+  ngOnInit(): void {}
 
-  ngOnInit(): void {
+  BuscarProducto(termino: string) {
+    if (termino.length < 1) {
+      return;
+    }
+
+    this.router.navigate(['/search', termino]);
   }
-
 }
